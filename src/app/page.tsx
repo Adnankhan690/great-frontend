83a0e67693f4ae34baafb7b604141fa58e987490
Adnan <adnan@Adnans-MacBook-Air.local>
2025-11-30 00:25:32 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+export default function Home() {
+	return (
+		<body className="font-sans">
+			<main>
+				<h1 className="text-5xl text-center font-semibold pt-20">
+					Hello world! Write your content here.
+				</h1>
+
+				{/* Sarah Dole
+          @sarahdole
+          I've been searching for high-quality abstract images for my design projects, 
+          and I'm thrilled to have found this platform. The variety and depth of creativity are astounding! 
+        */}
+
+				{/* Attribute this challenge to yourself! */}
+				<div className="credits" data-gfe-screenshot-exclude="true">
+					A challenge by
+					<a
+						href="https://www.greatfrontend.com/projects?ref=challenges"
+						target="_blank"
+						rel="noopener noreferrer">
+						{" "}
+						GreatFrontEnd Projects
+					</a>
+					. Built by
+					<a
+						href="https://www.greatfrontend.com/projects/u/username"
+						target="_blank"
+						rel="noopener noreferrer">
+						{" "}
+						Your Name
+					</a>
+					.
+				</div>
+			</main>
+		</body>
+	);
+}
